df921fa1dae2be4d075815fa1438ba6b0f92c9e4
aditirajaraman <aditirajaraman10272008@gmail.com>
2025-02-07 22:43:01 -0500

    Finalized Create Event Layout

--- src/pages/CreateEvent.tsx
+++ src/pages/CreateEvent.tsx
@@ -8,17 +8,23 @@ import { Image } from 'primereact/image';
 import { Panel } from 'primereact/panel';
 import { InputText } from 'primereact/inputtext';
 import { InputTextarea } from 'primereact/inputtextarea';
-import Button from 'react-bootstrap/Button';
 import { Calendar } from 'primereact/calendar';
 import { Dropdown } from 'primereact/dropdown';
+import { Button } from 'primereact/button';
+import { Divider } from 'primereact/divider';
 
 const eventLogo =  require("../assets/images/audience.jpg");
 
 const CreateEvent = () => {
-  const [value1, setValue1] = useState('');
+  const eventOptions = ['Single Event', 'Recurring Event'];
+  const [value1, setEventType] = useState('Single Event');
+  
+
   const [date8, setDate8] = useState<Date | Date[] | undefined>(undefined);
-  const options = ['Single Event', 'Recurring Event'];
+  
   const locationOptions = ['Venue', 'Online Event', 'To be Announced'];
+  const [eventValue, setLocatiob] = useState('Venue');
+
   const cities = [
     { name: 'Rome', code: 'RM' },
     { name: 'London', code: 'LDN' },
@@ -45,64 +51,148 @@ const CreateEvent = () => {
   ];
   return (
     <div className="container">
+       {/* ---------------------------Event Media --------------------- */}
        <Panel header="Event Media" toggleable>
         <Image src={eventLogo} alt="Image"/>
        </Panel>
-      <Panel header="Event Overview" toggleable>
-        <h3>Event Title</h3>
-        <InputText/>
-        <h3>Summary</h3>
-        <InputTextarea value={value1} onChange={(e) => setValue1(e.target.value)} rows={5} cols={30} />
+        {/* ---------------------------Event Details --------------------- */}
+      <Panel header="Event Overview" style={{fontWeight:'bold', fontSize:'20px'}} toggleable>
+      <div className="p-fluid grid formgrid">
+        <div className="field col-12 md:col-12">
+          <h5>Event Title</h5>
+          <InputText tooltip='Be clear and descriptive with a title that tells people what your event is about.'/>
+        </div>
+        <div className="field col-12 md:col-12">
+          <h5>Summary</h5>
+          <InputTextarea value={value1} rows={5} cols={80} autoResize 
+          tooltip='Attendees will see this at the top of your event page. (140 characters max)'/>
+        </div>
+      </div>
       </Panel>
-      <Panel header="Date Time and Location" toggleable>
-        <h3>Event Type</h3>
+       {/* ---------------------------Event Date Time and Location --------------------- */}
+      <Panel header="Date Time and Location" style={{fontWeight:'bold', fontSize:'20px'}} toggleable>
+        <h5>Event Type</h5>
         <div className="p-fluid grid formgrid">
           <div className="field col-12 md:col-12">
-            <SelectButton value={value1} options={options} onChange={(e) => setValue1(e.value)} />
+            <SelectButton value={value1} options={eventOptions} onChange={(e) => setEventType(e.value)} />
           </div>
           <div className="field col-12 md:col-4">
-              <label htmlFor="eventDate">Date</label>
+              <h5>Date</h5>
               <Calendar id="eventDate" />
           </div>
           <div className="field col-12 md:col-4">
-              <label htmlFor="startTime">Start Time</label>
+              <h5>Start Time</h5>
               <Calendar id="startTime" timeOnly hourFormat="12" />
           </div>
           <div className="field col-12 md:col-4">
-              <label htmlFor="endTime">End Time</label>
+              <h5>End Time</h5>
               <Calendar id="endTime" timeOnly hourFormat="12" />
           </div>
           <div className="field col-12 md:col-8">
-              <h3>Location</h3>
-              <SelectButton value={value1} options={locationOptions} onChange={(e) => setValue1(e.value)} />
+              <h5>Location</h5>
+              <SelectButton value={eventValue} options={locationOptions} onChange={(e) => setLocatiob(e.value)} />
           </div>
           <div className="field col-12 md:col-4">
           </div>
           <div className="field col-12 md:col-8">
-              <label htmlFor="Address">Address</label>
+              <h5>Address</h5>
               <InputText/>
           </div>
           <div className="field col-12 md:col-4">
-            <label htmlFor="cities">City</label>
+            <h5>City</h5>
             <Dropdown options={cities} optionLabel="name" placeholder="Select a City" />
           </div>
           <div className="field col-12 md:col-4">
-            <label htmlFor="states">State</label>
-            <Dropdown options={states} optionLabel="name" placeholder="Select a City" />
+            <h5>State</h5>
+            <Dropdown options={states} optionLabel="name" placeholder="Select a State" />
           </div>
           <div className="field col-12 md:col-4">
-            <label htmlFor="countries">Country</label>
+            <h5>Country</h5>
             <Dropdown options={countries} optionLabel="name" placeholder="Select a Country" />
           </div>
           <div className="field col-12 md:col-4">
-            <label htmlFor="Address">ZipCode</label>
+            <h5>Zip</h5>
             <InputText/>
           </div>
           
         </div>
         
       </Panel>
-     
+       {/* ---------------------------Event Itenary---------------------------------------- */}
+       <Panel header="Event Itenary" style={{fontWeight:'bold', fontSize:'20px'}} toggleable>
+       <div className="p-fluid grid formgrid">
+          <div className="field col-12 md:col-12">
+            <h5>Host/Artist</h5>
+            <InputText/>
+          </div>
+          <div className="field col-12 md:col-6">
+            <h5>Title</h5>
+            <InputText/>
+          </div>
+          <div className="field col-12 md:col-6">
+            <h5>Description</h5>
+            <InputTextarea value={value1} rows={5} cols={80} autoResize tooltip='List your Agenda'/>
+          </div>
+          <div className="field col-12 md:col-4">
+            <h5>Start Time</h5>
+            <Calendar id="agendaStartTime" timeOnly hourFormat="12" />
+          </div>
+          <div className="field col-12 md:col-4">
+            <h5>End Time</h5>
+            <Calendar id="agendaEndTime" timeOnly hourFormat="12" />
+          </div>
+        </div>
+       </Panel>
+       {/* ---------------------------Event FAQS------------------------------------------- */}
+       <Panel header="Event FAQS" style={{fontWeight:'bold', fontSize:'20px'}} toggleable>
+        <div className="p-fluid grid formgrid">
+          <div className="field col-12 md:col-6">
+          </div>
+          <div className="field col-12 md:col-6">
+            <Button icon="pi pi-plus" rounded severity="warning" aria-label="Notification"  style={{float:'right'}}/>
+          </div>
+          <Divider align="left">
+            <div className="inline-flex align-items-center">
+                <b>FAQ 1</b>
+            </div>
+          </Divider>
+          <div className="field col-12 md:col-12">
+              <div style={{width: '100%', display:'table'}}>
+                  <div style={{display: 'table-row'}}>
+                      <div style={{width: '600px', display: 'table-cell'}}><h5>Question</h5></div>
+                      <div style={{display: 'table-cell'}}> 
+                          <Button icon="pi pi-trash" rounded severity="danger" size="small" style={{float:'right'}}/>
+                      </div>
+                  </div>
+              </div>
+              <InputText/>
+          </div>
+          <div className="field col-12 md:col-12">
+            <h5>Answer</h5>
+            <InputTextarea rows={5} cols={80} autoResize />
+          </div>
+          <Divider align="left">
+            <div className="inline-flex align-items-center">
+                <b>FAQ 2</b>
+            </div>
+          </Divider>
+          <div className="field col-12 md:col-12">
+              <div style={{width: '100%', display:'table'}}>
+                  <div style={{display: 'table-row'}}>
+                      <div style={{width: '600px', display: 'table-cell'}}><h5>Question</h5></div>
+                      <div style={{display: 'table-cell'}}> 
+                          <Button icon="pi pi-trash" rounded severity="danger" size="small" style={{float:'right'}}/>
+                      </div>
+                  </div>
+              </div>
+              <InputText/>
+          </div>
+          <div className="field col-12 md:col-12">
+            <h5>Answer</h5>
+            <InputTextarea rows={5} cols={80} autoResize />
+          </div>
+        </div>
+       </Panel>
     </div>
   );
 };
