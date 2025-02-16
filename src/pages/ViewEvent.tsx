c6042b3be6720555ba1d04879cba165976a10207
aditirajaraman <aditirajaraman10272008@gmail.com>
2025-02-15 23:26:13 -0500

    Included Withdraw , Resume and Cancel Events - UI Elements

--- src/pages/ViewEvent.tsx
+++ src/pages/ViewEvent.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Button from 'react-bootstrap/Button';
 import axios from 'axios';
 import { Accordion, AccordionTab } from 'primereact/accordion';
+import { SelectButton } from 'primereact/selectbutton';
 
 const ViewEvent = ({eventData}) => {
     const [currentEvent, setCurrentEvent] = useState(null);
@@ -21,7 +22,8 @@ const ViewEvent = ({eventData}) => {
                 console.log(err);
         });
     }, []);  
-    
+    const [eventState, setEventState] = useState('Withdraw');
+    const eventStateOptions = ['Withdraw Event', 'Resume Event', 'Cancel Event'];
     return (
     <div>
         <Accordion activeIndex={0} multiple>
@@ -56,6 +58,13 @@ const ViewEvent = ({eventData}) => {
             <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi
                 architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
                 voluptatem sequi nesciunt. Consectetur, adipisci velit, sed quia non numquam eius modi.</p>
+            </AccordionTab>
+            <AccordionTab header="Event Management">
+            <div className="grid">
+                <div className=" col-12 md:col-12">
+                    <SelectButton value={eventState} options={eventStateOptions} onChange={(e) => setEventState(e.value)} />
+                </div>
+            </div>
             </AccordionTab>
             <AccordionTab header="FAQs">
             <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati
