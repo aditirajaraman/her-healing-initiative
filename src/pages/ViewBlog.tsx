b521e588625d030bd20c04ddb4f9f44fab8a3a24
aditirajaraman <aditirajaraman10272008@gmail.com>
2025-08-20 23:47:17 -0400

    included speed dial

--- src/pages/ViewBlog.tsx
+++ src/pages/ViewBlog.tsx
@@ -1,5 +1,5 @@
 /*********************************1: Imports / react *************************************/
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useLocation } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import axios from 'axios';
@@ -9,12 +9,16 @@ import { Accordion, AccordionTab } from 'primereact/accordion';
 import { SelectButton } from 'primereact/selectbutton';
 import { Card } from 'primereact/card';
 import { Avatar } from 'primereact/avatar';
-import { Tooltip } from 'primereact/tooltip';
 import { Badge } from 'primereact/badge';
 import { Divider } from 'primereact/divider';
 import { Chip } from 'primereact/chip';
+import { SpeedDial } from 'primereact/speeddial';
+import { Tooltip } from 'primereact/tooltip';
+import { Toast } from 'primereact/toast';
+
 
 /*********************************3: Imports / custom css ********************************/
+import '../assets/css/speeddial.css'
 
 /*********************************4: Imports config ********************************/
 const config = require('../config/config_' + process.env.NODE_ENV?.trim() + '.json');
@@ -38,6 +42,45 @@ const ViewBlog: React.FC = () => {
 
   const location = useLocation();
   const currentBlogState = location.state?.blogData as BlogData;
+
+  const toast = useRef<any>(null);
+  const items = [
+        {
+            label: 'Send',
+            icon: 'pi pi-send',
+            command: () => {
+                toast.current.show({ severity: 'info', summary: 'Add', detail: 'Data Added' });
+            }
+        },
+        {
+            label: 'Save',
+            icon: 'pi pi-save',
+            command: () => {
+                toast.current.show({ severity: 'success', summary: 'Update', detail: 'Data Updated' });
+            }
+        },
+        {
+            label: 'tag',
+            icon: 'pi pi-tag',
+            command: () => {
+                toast.current.show({ severity: 'error', summary: 'Delete', detail: 'Data Deleted' });
+            }
+        },
+        {
+            label: 'Bookmark',
+            icon: 'pi pi-bookmark',
+            command: () => {
+                window.location.hash = "/fileupload"
+            }
+        },
+        {
+            label: 'Share',
+            icon: 'pi-whatsapp',
+            command: () => {
+                window.location.href = 'https://facebook.github.io/react/'
+            }
+        }
+    ];
 
   // The 'blogData' is probably just the ID, which is safer to get from the URL
   // If possible, use useParams() instead: const { id } = useParams();
@@ -94,23 +137,27 @@ const ViewBlog: React.FC = () => {
 
   return (
     <div className="w-90" style={{padding:'2rem'}}>
+      <Toast ref={toast} />
       <div className="grid">
         {/* ---------------------------------Banner------------------------------------ */} 
         <div className="col-12">
             <img src={currentBlog.blogImage} style={{width:'100%', height:'200px'}}/>
         </div> 
         {/* ----------------------------------Title--------------------------------------- */}
-        <div className="col-12"><h1 style={{textAlign:'center'}}>{currentBlog.title}</h1></div>
+        <div className="col-11"><h1 style={{textAlign:'center'}}>{currentBlog.title}</h1></div>
+        <div className="col-1"> <SpeedDial model={items} radius={80} direction="down" type="semi-circle" className="p-speeddial-rounded" /></div>
       </div>
       {/* ----------------------------------User Blog Profile-------------------------- */}
       <div className="flex justify-content-center flex-wrap">
-          <div className="flex align-items-center justify-content-center">
-            <Avatar image={require( `../assets/images/event-organizers/${currentBlog.authorIcon}.png`)} className="mr-2" size="normal" shape="circle"/>
-          </div>
-          <div className="flex align-items-center justify-content-center">
-            <span style={{verticalAlign:'middle', margin:'15px',fontFamily:'Lucida Handwriting', fontSize:'15px'}}><b>{"Aditi Rajaraman"}</b></span> 
-          </div>
+        <div className="flex align-items-center justify-content-center">
+          <Avatar image={require( `../assets/images/event-organizers/${currentBlog.authorIcon}.png`)} className="mr-2" size="normal" shape="circle"/>
+        </div>
+        <div className="flex align-items-center justify-content-center">
+          <span style={{verticalAlign:'middle', margin:'15px',fontFamily:'Lucida Handwriting', fontSize:'15px'}}><b>{"Aditi Rajaraman"}</b></span> 
+        </div>
       </div>
+      {/* ----------------------------------User Blog Profile-------------------------- */}
+      <Divider />
     </div>
   );
 };
