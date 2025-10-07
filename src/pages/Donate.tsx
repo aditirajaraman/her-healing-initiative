c995b3cc10a8fa3d2c8e214a3d9b8e6f49afe5e8
aditirajaraman <aditirajaraman10272008@gmail.com>
2025-10-06 22:32:29 -0400

    Set donation freq

--- src/pages/Donate.tsx
+++ src/pages/Donate.tsx
@@ -1,21 +1,178 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+/*********************************1: Imports / react *************************************/
+import React, { useState, useEffect, useRef } from 'react';
+import axios from 'axios';
+
+/*********************************2: Imports / primereact ********************************/
+import { Card } from 'primereact/card';
 import { Button } from 'primereact/button';
+import { SelectButton } from 'primereact/selectbutton';
+import { Dropdown } from 'primereact/dropdown';
+import { InputText } from 'primereact/inputtext';
+import { InputTextarea } from 'primereact/inputtextarea';
+import { InputMask } from 'primereact/inputmask';
+import { useForm, Controller } from 'react-hook-form';
+import { classNames } from 'primereact/utils';
+
+/*********************************3: Imports / custom css ********************************/
+import '../assets/css/donate.css';
+
+/*********************************3: Imports / configs ********************************/
+const config = require('../config/config_' + process.env.NODE_ENV?.trim() + '.json');
+
+/*********************************4: Component View ********************************/
 
 const Donate = () => {
-  return (
-    <div>
-      <h1>About Donation</h1>
-      <div className="p-fluid grid formgrid">
-          <div className="field col-12 md:col-12">We’re on a mission to end the global water crisis and committed to demonstrating God's love by restoring hope and health to under-served populations through trusted, sustainable safe water solutions and health & hygiene education.</div>
-          <div className="field col-3 md:col-3"></div> 
-          <div className="field col-3 md:col-3"> <Button label="One-Time" className="p-button-raised" /></div> 
-          <div className="field col-3 md:col-3"><Button label="Recurring"  className="p-button-rounded p-button-secondary" /></div> 
-          <div className="field col-3 md:col-3"></div> 
-      </div>
-    </div>
+    /*---------------------4.1: Navigators ---------------------*/
 
-  );
+    /*---------------------4.2: Variablea ---------------------*/
+
+     const donationFrequencies = [
+        { name: 'Weekly', code: 'Weekly' },
+        { name: 'Bi-Weekly', code: 'Bi-Weekly' },
+        { name: 'Monthly', code: 'Monthly' },
+        { name: 'Quarterly', code: 'Quarterly' },
+        { name: 'Semi-Annually', code: 'Semi-Annually' },
+        { name: 'Year', code: 'Yearly' }
+    ];
+    const donationReasons = [
+        { name: 'Rapid Response Fund', code: 'RRF' },
+        { name: 'Her Healing Initiative', code: 'HHI' },
+        { name: 'United Nations Womens Fund', code: 'UNfund' }
+    ];
+
+    const defaultValues = {
+        donationType: '',
+        donationFrequency: donationFrequencies[0],
+        donationReason: '',
+        firstName: '',
+        lastName: '',
+        email: '',
+        address: '',
+        city: '',
+        zip:null,
+        state: '',
+        country: '',
+        phonenumber:'',
+        comments:'',
+        otherAmount:null
+    }
+
+   
+
+    /*---------------------4.3: State Management ------------------------*/
+    const options = ['One-Time', 'Recurring'];
+    const [currencies, setCurrencies] = useState([]);
+    const [selectedCurrency, setSelectedCurrency] = useState<any>(null);
+    const [donationType, setDonationType] = useState<"One-Time" | "Recurring">("Recurring");
+    const [selectedPaymentFrequency, setSelectedPaymentFrequency] = useState<any>(donationFrequencies[0]);
+    const {control, register, formState: { errors }, handleSubmit, reset } = useForm({defaultValues});
+    const [formData, setFormData] = useState({});
+
+    /*---------------------4.4: Control References ---------------------*/
+    const getFormErrorMessage = (name) => {
+        return errors[name] && <small className="p-error">{errors[name].message}</small>
+    };
+
+    /*---------------------4.5: Control Event Handlers -------------------*/
+    const setDonationFrequencyChange = (e: {value: any}) => {
+        console.log("-------------payment frequency changed----------");
+        
+        setSelectedPaymentFrequency(e.value);
+        console.log(e.value);
+    }
+
+    const onCurrencyChange = (e: {value: any}) => {
+        setSelectedCurrency(e.value);
+    }
+    
+    const countryOptionTemplate = (option: any) => {
+    return (
+        <div className="country-item">
+            <div>{option.code} | {option.country}</div>
+        </div>
+    );
+    }
+
+    const onSubmit = (formData) => {
+        setFormData(formData);
+
+    //console.log('---------------------formdata-----------------');
+    }
+
+    /*---------------------4.: UI Controls Reference ---------------------*/ 
+    
+    
+    const formRef = useRef(null);
+
+    /*---------------------4.7: React Hooks for Component OnLoad()  ---------------------*/
+    useEffect(() => {
+      const apiKey = process.env.API_URL;
+      console.log(apiKey);
+      axios({
+        // Endpoint to send files
+        url: config.API_URL + "/currencies",
+        method: "GET"
+        })
+        .then((res) => {
+            //console.log("--------------logged---------------");
+            setCurrencies(res.data);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+    return (
+        <div style={{padding: '5rem'}}>
+            <Card title = "Donation" style={{borderRadius: '5px', border: '1px solid #dcdcdc', boxShadow: '0 2px 8px rgba(2, 2, 2, 0.3)'}}>
+                <form ref={formRef} onSubmit={handleSubmit(onSubmit)} className="p-fluid">
+                <div className="lex justify-content-center flex-wrap">
+                    <div className="flex align-items-center justify-content-center pt-2">
+                        <h6> We’re on a mission to end the global water crisis and committed to demonstrating God's love by restoring hope and health to under-served populations through trusted, sustainable safe water solutions and health & hygiene education.</h6>
+                    </div>
+                </div>
+                <div className="p-fluid grid formgrid pt-5">
+                    <div className="field col-3 md:col-3"></div>
+                    <div className="field col-6 md:col-6">
+                        <span className="p-float-label">  
+                            <Controller name="donationType" control={control} rules={{ required: 'Donation type is required.' }} render={({ field, fieldState }) => (
+                                <SelectButton id={field.name} {...field} autoFocus className={classNames({ 'p-invalid': fieldState.invalid })} value={donationType} onChange={(e) => setDonationType(e.value)} options={options}/>
+                            )} />
+                        </span>
+                        {getFormErrorMessage('donationType')}
+                    </div> 
+                    <div className="field col-3 md:col-3"></div>
+                </div>
+                    {donationType === "Recurring" && (
+                    <div>
+                        <div className="field col-12 md:col-12">
+                            <span className="p-float-label">
+                                <Controller name="donationFrequency" control={control} rules={{ required: 'Donation frequency is required.' }} render={({ field}) => (  
+                                    <Dropdown id={field.name} value={field.value} options={donationFrequencies} optionLabel="name" 
+                                    onChange={(e) => {
+                                        field.onChange(e.value);              // update form value
+                                        setSelectedPaymentFrequency(e.value); // update local state for display
+                                    }}/>
+                                )} />
+                                <label htmlFor="donationFrequency" className={classNames({ 'p-error': errors.donationFrequency })} style={{fontSize: '15px'}}>Donation Frequency*</label>
+                            </span>
+                            {getFormErrorMessage('donationFrequency')}
+                        </div>
+                        <div className="field col-12 md:col-12" style={{ textAlign: 'center' }}>
+                            Choose a&nbsp;<b>{selectedPaymentFrequency.name}</b>&nbsp;amount.
+                        </div>
+                    </div>
+                    )}
+                    {donationType === "One-Time" && (
+                        <div className="field col-12 md:col-12">
+                            Choose a &nbsp;<b>one-time</b>&nbsp;amount.
+                        </div>
+                    )}
+
+                </form>
+            </Card>
+        </div>
+    );
 };
 
 export default Donate;
